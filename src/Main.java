59374a061f29d485a848d793df361509e8924e48
Angel97Figueroa <angel.figueroa97@uabc.edu.mx>
2026-04-16 09:03:53 -0700

    Se agregó el metodo multiplicador en la clase Numeros y un ejemplo de uso en la clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class Main{
     public static void main(){
-
+        //Ejemplo de mulplicador
+        ArrayList<Integer> nums=new ArrayList<>();
+        for(int i=1;i<=5;i++){
+            nums.add(i);
+        }
+        nums=Numeros.multiplicador(nums, 5);
+        System.out.println(nums);
     }
 }
