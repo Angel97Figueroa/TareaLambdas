7a49c3e105c330912842de31805b30fb29dd8960
Angel97Figueroa <angel.figueroa97@uabc.edu.mx>
2026-04-17 15:29:07 -0700

    Se agregó al main un ejemplo de uso de cada metodo verificando que no se rompe al pasarle listas o mapas con valores nulos

--- src/UtileriaCadenas.java
+++ src/UtileriaCadenas.java
@@ -3,35 +3,45 @@ import java.util.stream.Collectors;
 
 public class UtileriaCadenas {
     public static ArrayList<String> filtroSelectvo(char letraProhibida, int longitudMin, ArrayList<String> wordsList){
-        wordsList.removeIf(cadena->cadena.toLowerCase().startsWith(String.valueOf(letraProhibida).toLowerCase()) || cadena.length()<longitudMin);
+        wordsList.removeIf(cadena->cadena==null || cadena.toLowerCase().startsWith(String.valueOf(letraProhibida).toLowerCase()) || cadena.length()<longitudMin);
         return wordsList;
     }
 
     public static ArrayList<String> conversorDeMayusculas(ArrayList<String> wordsList){
-        return wordsList.stream().map(cadena->cadena.toUpperCase()).collect(Collectors.toCollection(ArrayList<String>::new));
+        return wordsList.stream().filter(cadena->cadena!=null).map(cadena->cadena.toUpperCase()).collect(Collectors.toCollection(ArrayList<String>::new));
     }
 
     public static HashMap<String, Integer> mapaDeLongitudes(ArrayList<String> wordsList){
-        return wordsList.stream().collect(Collectors.toMap(w->w,w->w.length(),(viejo, nuevo)->viejo,HashMap::new));
+        return wordsList.stream().filter(s->s!=null).collect(Collectors.toMap(w->w,w->w.length(),(viejo, nuevo)->viejo,HashMap::new));
     }
 
     public static HashMap<String, Integer> contadorDeFrecuencias(ArrayList<String> wordsList){
         HashMap<String, Integer> mapaDeFrecuencias=new HashMap<>();
-        wordsList.forEach(w->mapaDeFrecuencias.merge(w,1,(viejo, nuevo)->viejo+nuevo));
+        wordsList.stream().filter(cadena->cadena!=null).forEach(w->mapaDeFrecuencias.merge(w,1,(viejo, nuevo)->viejo+nuevo));
         return mapaDeFrecuencias;
     }
 
     public static ArrayList<String> clasificadorDePalabras(int frecuenciaMin, HashMap<String, Integer> mapaDeFrecuencias){
-        ArrayList<String> palabrasClasificadas=mapaDeFrecuencias.entrySet().stream().filter(elemento->elemento.getValue()>frecuenciaMin).map(e->e.getKey()).collect(Collectors.toCollection(ArrayList<String>::new));
+        ArrayList<String> palabrasClasificadas=mapaDeFrecuencias.entrySet().stream().filter(elemento->elemento.getValue()!=null && elemento.getKey()!=null).filter(elemento->elemento.getValue()>frecuenciaMin).map(e->e.getKey()).collect(Collectors.toCollection(ArrayList<String>::new));
         return palabrasClasificadas;
     }
 
     public static HashSet<String> dedupliacionDePalabras(String frase, int minimoDeLetras){
-        return Arrays.stream(frase.split(" ")).filter(palabra->palabra.toLowerCase().length()<minimoDeLetras).collect(Collectors.toCollection(HashSet<String>::new));
+        if(frase==null){
+            return new HashSet<>();
+        }
+        return Arrays.stream(frase.split("\\W+")).filter(palabra->palabra!=null).filter(palabra->!palabra.isEmpty()).filter(palabra->palabra.toLowerCase().length()<minimoDeLetras).collect(Collectors.toCollection(HashSet<String>::new));
     }
 
     public static HashMap<String, Integer> topeDeFrecuencia(HashMap<String, Integer> mapaDeFrecuencias, int limiteDeFrecuencia){
-        mapaDeFrecuencias.replaceAll((palabra, frecuencia)->frecuencia>=limiteDeFrecuencia ? limiteDeFrecuencia : frecuencia);
+        mapaDeFrecuencias.replaceAll((palabra, frecuencia)->{
+            if(frecuencia!=null && palabra!=null){
+                frecuencia=frecuencia>=limiteDeFrecuencia ? limiteDeFrecuencia : frecuencia.intValue();
+            }else{
+                return frecuencia;
+            }
+            return frecuencia;
+        });
         return mapaDeFrecuencias;
     }
 
--- src/UtileriaNumeros.java
+++ src/UtileriaNumeros.java
@@ -1,24 +1,22 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class UtileriaNumeros {
     public static ArrayList<Integer> multiplicador(ArrayList<Integer> listaNums, int factor){
+        listaNums.removeIf(n->n==null); //Se quitan los valores nulos del ArrayList
         listaNums.replaceAll(num->num.intValue()*factor);
         return listaNums;
     }
 
     public static HashSet<Integer> cuadradosUnicos(ArrayList<Integer> listaNums){
-        HashSet<Integer> listaNumsUnicos=(HashSet<Integer>)listaNums.stream().filter(num->num%2==0).map(num->num*num).collect(Collectors.toSet());
+        HashSet<Integer> listaNumsUnicos=(HashSet<Integer>)listaNums.stream().filter(n->n!=null).filter(num->num%2==0).map(num->num*num).collect(Collectors.toSet());
         System.out.println("Numeros originales\n"+listaNums); //ArrayList sin modificar
         return listaNumsUnicos;
     }
 
     public static void modificadorDeInventario(HashMap<String, Double> inventario){
         System.out.println("Mapa con el descuento aplicado");
-        inventario.forEach((producto, precio)->System.out.println(producto+" "+precio.doubleValue()*0.9));
+        inventario.entrySet().stream().filter(elemento->elemento.getValue()!=null && elemento.getKey()!=null).forEach(elemento->System.out.println(elemento.getKey()+" "+elemento.getValue().doubleValue()*0.9));
         System.out.println(" ");
     }
 }
