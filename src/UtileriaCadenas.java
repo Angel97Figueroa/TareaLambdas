3afdb57d38fbec2b41a41ddd77173f76f5d4f46c
Angel97Figueroa <angel.figueroa97@uabc.edu.mx>
2026-04-17 16:38:10 -0700

    Se agregaron medidas de seguridad para que no truene al enviarle listas o mapas nulos

--- src/UtileriaCadenas.java
+++ src/UtileriaCadenas.java
@@ -3,30 +3,55 @@ import java.util.stream.Collectors;
 
 public class UtileriaCadenas {
     public static ArrayList<String> filtroSelectvo(char letraProhibida, int longitudMin, ArrayList<String> wordsList){
+        //Si el ArrayList es nulo, se termina el metodo regresando un ArrayList vacio
+        if(wordsList==null){
+            return new ArrayList<>();
+        }
         wordsList.removeIf(cadena->cadena==null || cadena.toLowerCase().startsWith(String.valueOf(letraProhibida).toLowerCase()) || cadena.length()<longitudMin);
         return wordsList;
     }
 
     public static ArrayList<String> conversorDeMayusculas(ArrayList<String> wordsList){
+        //Si el ArrayList es nulo, se termina el metodo regresando un ArrayList vacio
+        if(wordsList==null){
+            return new ArrayList<>();
+        }
+
         return wordsList.stream().filter(cadena->cadena!=null).map(cadena->cadena.toUpperCase()).collect(Collectors.toCollection(ArrayList<String>::new));
     }
 
     public static HashMap<String, Integer> mapaDeLongitudes(ArrayList<String> wordsList){
+        //Si el HashMap es nulo, se termina el metodo regresando un HashMap vacio
+        if(wordsList==null){
+            return new HashMap<>();
+        }
+
         return wordsList.stream().filter(s->s!=null).collect(Collectors.toMap(w->w,w->w.length(),(viejo, nuevo)->viejo,HashMap::new));
     }
 
     public static HashMap<String, Integer> contadorDeFrecuencias(ArrayList<String> wordsList){
+        //Si el HashMap es nulo, se termina el metodo regresando un HashMap vacio
+        if(wordsList==null){
+            return new HashMap<>();
+        }
+
         HashMap<String, Integer> mapaDeFrecuencias=new HashMap<>();
         wordsList.stream().filter(cadena->cadena!=null).forEach(w->mapaDeFrecuencias.merge(w,1,(viejo, nuevo)->viejo+nuevo));
         return mapaDeFrecuencias;
     }
 
     public static ArrayList<String> clasificadorDePalabras(int frecuenciaMin, HashMap<String, Integer> mapaDeFrecuencias){
+        //Si el HashMap es nulo, se termina el metodo regresando un ArrayList vacio
+        if(mapaDeFrecuencias==null){
+            return new ArrayList<>();
+        }
+
         ArrayList<String> palabrasClasificadas=mapaDeFrecuencias.entrySet().stream().filter(elemento->elemento.getValue()!=null && elemento.getKey()!=null).filter(elemento->elemento.getValue()>frecuenciaMin).map(e->e.getKey()).collect(Collectors.toCollection(ArrayList<String>::new));
         return palabrasClasificadas;
     }
 
     public static HashSet<String> dedupliacionDePalabras(String frase, int minimoDeLetras){
+        //Si la frase es nula, se termina el metodo regresando un HashSet
         if(frase==null){
             return new HashSet<>();
         }
@@ -34,6 +59,11 @@ public class UtileriaCadenas {
     }
 
     public static void topeDeFrecuencia(HashMap<String, Integer> mapaDeFrecuencias, int limiteDeFrecuencia){
+        //Si el HashMap es nulo, se termina el metodo
+        if(mapaDeFrecuencias==null){
+            System.out.println("El HashMap enviado es null, no se trabajó con él");
+            return;
+        }
         mapaDeFrecuencias.replaceAll((palabra, frecuencia)->{
             if(frecuencia!=null && palabra!=null){
                 frecuencia=frecuencia>=limiteDeFrecuencia ? limiteDeFrecuencia : frecuencia.intValue();
--- src/UtileriaNumeros.java
+++ src/UtileriaNumeros.java
@@ -3,18 +3,34 @@ import java.util.stream.Collectors;
 
 public class UtileriaNumeros {
     public static ArrayList<Integer> multiplicador(ArrayList<Integer> listaNums, int factor){
+        //Si el ArrayList es nulo, se termina el metodo regresando un ArrayList vacio
+        if(listaNums==null){
+            return new ArrayList<>();
+        }
+
         listaNums.removeIf(n->n==null); //Se quitan los valores nulos del ArrayList
         listaNums.replaceAll(num->num.intValue()*factor);
         return listaNums;
     }
 
     public static HashSet<Integer> cuadradosUnicos(ArrayList<Integer> listaNums){
+        //Si el ArrayList es nulo, se termina el metodo regresando un HashSet vacio
+        if(listaNums==null){
+            return new HashSet<>();
+        }
+
         HashSet<Integer> listaNumsUnicos=(HashSet<Integer>)listaNums.stream().filter(n->n!=null).filter(num->num%2==0).map(num->num*num).collect(Collectors.toSet());
         System.out.println("Numeros originales\n"+listaNums); //ArrayList sin modificar
         return listaNumsUnicos;
     }
 
     public static void modificadorDeInventario(HashMap<String, Double> inventario){
+        //Si el HashMap es nulo, se termina el metodo
+        if(inventario==null){
+            System.out.println("El HashMap enviado es null, no se trabajó con él");
+            return;
+        }
+
         System.out.println("Mapa con el descuento aplicado");
         inventario.entrySet().stream().filter(elemento->elemento.getValue()!=null && elemento.getKey()!=null).forEach(elemento->System.out.println(elemento.getKey()+" "+elemento.getValue().doubleValue()*0.9));
         System.out.println(" ");
